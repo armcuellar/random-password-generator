11e57604af3aeedfdbaeedf91063efd990e69614
Armando <cuelarmando@yahoo.com>
2021-12-18 13:59:08 -0500

    added password length with validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  // resets results when generating new password
   var results = "";
+
+  // Calls password length function and return valid length number
+  var validLength = passwordLength();
+  console.log(validLength);
+
+  // ask for criteria
   var numeric = window.confirm("Would you like numeric characters?");
   var upperCase = window.confirm("Would you like uppercase characters?");
   var lowerCase = window.confirm("Would you like lowercase characters?");
   var special = window.confirm("Would you like special characters?");
 
-
+  // generates random characters
   if (numeric) {
     results = results + generateNumber();
   }
@@ -29,12 +36,44 @@ function generatePassword() {
 
 }
 
-// returns random number from 0-9
+// Function to ask for password length
+var passwordLength = function () {
+  correctLength = false;
+
+  //validates for a correct value 
+  while (correctLength === false) {
+    var askLength = window.prompt("How long would you like your password? enter a value from 8 to 128")
+
+    if (askLength < 8) {
+      window.alert("Password length is too short please select a number between 8 and 128.")
+    }
+    else if (askLength > 128) {
+      window.alert("Password length is too long please select a number between 8 and 128.")
+    }
+    // test whether the prompt is a number
+    else if (isNaN(askLength)) {
+      window.alert("Please enter a valid number.")
+    }
+    else {
+      correctLength = true;
+    }
+  }
+
+  // console log to see value
+  console.log(askLength);
+
+  // return value back to passwordLength
+  return askLength;
+
+}
+
+// generates random number from 0-9
 function generateNumber() {
   var numberGenerated = Math.floor(Math.random() * 10);
   return numberGenerated;
 }
 
+// generates random uppercase characters
 function generateUpperCase() {
   var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var charactersLength = characters.length;
@@ -43,6 +82,7 @@ function generateUpperCase() {
   return characterGenerated;
 
 }
+// generates random lowercase characters
 function generateLowerCase() {
   var characters = "abcdefghijklmnopqrstuvwxyz";
   var charactersLength = characters.length;
@@ -50,6 +90,7 @@ function generateLowerCase() {
 
   return characterGenerated;
 }
+// generates random special character
 function generateSpecial() {
   var characters = "!@#$%^&*()[]{}";
   var charactersLength = characters.length;
@@ -65,7 +106,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 
 
 // Add event listener to generate button
